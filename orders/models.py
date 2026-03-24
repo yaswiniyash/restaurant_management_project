238d49a6d2bacd85c64b4e645e359057fb0830cc
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-24 18:56:02 +0530

    68159

--- orders/models.py
+++ orders/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.name
