fbc36d6cb501524c0974333de8d1e983a6b1eed9
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-24 19:13:55 +0530

    68158

--- orders/models.py
+++ orders/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class OrderStatus(models.Model):
-    name = models.CharField(max_length=50,unique = True)
+    name = models.CharField(max_length=50,unique=True)
     def __str__(self):
         return self.name
 
 class Order(models.Model):
-    status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null = True,blank = True)
+    status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True,blank=True)
     def __str__(self):
-        return f"Order {self.id}-{self.status}"
+        return f"Order #{self.id} - Status{self.status}"
