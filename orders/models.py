96e6c9160493dee5296f9e17a2fa1df035c46b43
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-24 19:31:43 +0530

    68160

--- orders/models.py
+++ orders/models.py
@@ -6,7 +6,18 @@ class OrderStatus(models.Model):
     def __str__(self):
         return self.name
 
+
 class Order(models.Model):
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True,blank=True)
     def __str__(self):
         return f"Order #{self.id} - Status{self.status}"
+
+
+class Coupon(models.Model):
+    code = models.CharField(unique=True)
+    discount_percentage = models.DecimalField(max_digits=5,decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    valid_from = models.DateField()
+    valid_until = models.DateField()
+    def __str__(self):
+        return f"{self.code} - {self.discount_percentage}%"
