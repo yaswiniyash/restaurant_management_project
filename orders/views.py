44aadf121d15b72b5b04c683ba87bdd5332731a7
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-25 22:15:56 +0530

    68236

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.views import APIview
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
