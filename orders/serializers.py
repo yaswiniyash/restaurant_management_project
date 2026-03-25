543b07656453d0f1747bff83f918847cc5febec6
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-25 22:43:12 +0530

    68236

--- orders/serializers.py
+++ orders/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from .models import Order, OrderItem
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    model = OrderItem
-    fields = ['product_item', 'quantity', 'price']
+    class Meta:
+        model = OrderItem
+        fields = ['product_item', 'quantity', 'price']
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
 
-class Meta:
-    model = Order
-    fields = ['id', 'created_at', 'total_price', 'items']
+    class Meta:
+        model = Order
+        fields = ['id', 'created_at', 'total_price', 'items']
