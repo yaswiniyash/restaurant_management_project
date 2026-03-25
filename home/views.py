cb315e4fe7f04daa2f9e2324d956e8bb085d25a7
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-25 21:31:38 +0530

    68235

--- home/views.py
+++ home/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import MenuCategory
 from .serializers import MenuCategorySerializer
+from .utils import get_today_operating_hours
+from django.http import HttpResponse
 
 # Create your views here.
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+
+def show_today_hours(request):
+    open_time, close_time = get_today_operating_hours()
+
+    if open_time and close_time:
+        return HttpResponse(f"Today open from {open_time} - {close_time}")
+    else:
+        return HttpResponse('Resturant is closed today')
