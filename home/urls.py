aea6873f7907880c5cd411481772a91c2c1a6731
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-26 21:03:46 +0530

    68237

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("categories/",MenuCategoryListView.as_view(),name="menu-categories"),
     path("today-hours/",show_today_hours),
+    path('featured-menu/', FeaturedMenuItemListView.as_view(), name='featured-menu')
 ]
