f341bcc615a04a17caf6d285aa04cf5a2fd127b5
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-26 20:49:48 +0530

    68237

--- home/models.py
+++ home/models.py
@@ -14,3 +14,12 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_length=10,decimal_places=2)
+    description = models.TextField(blank=True)
+    is_featured = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
