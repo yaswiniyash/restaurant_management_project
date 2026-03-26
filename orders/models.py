7fa8f0a8b82ac5217e2d9f7ce06967e91b9d6069
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-26 20:50:31 +0530

    68237

--- orders/models.py
+++ orders/models.py
@@ -9,8 +9,8 @@ class OrderStatus(models.Model):
 
 class Order(models.Model):
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True,blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True,blank=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     def __str__(self):
         return f"Order #{self.id} - Status{self.status}"
