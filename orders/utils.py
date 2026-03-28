178dc9db6d4701f3d8ef41f3cfd69c75aa659e92
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-28 21:42:44 +0530

    68353

--- orders/utils.py
+++ orders/utils.py
@@ -3,6 +3,7 @@ import secrets
 from .models import Coupon
 from django.db.models import Sum
 from .models import Order
+from decimal import Decimal, ROUND_HALF_UP
 
 def generate_coupon_code(length=10):
     characters = string.ascii_uppercase + string.digits
@@ -17,3 +18,10 @@ def get_daily_sales_total(date):
     orders = Order.objects.filter(Created_at__date=date)
     total = orders.aggregate(total_sum=Sum('total_price'))['total_sum']
     return total if total is not None else 0
+
+def calculate_tip_amount(order_total, tip_percentage):
+    order_total = Decimal(order_total)
+    tip_percentage = Decimal(tip_percentage)
+    tip_amount = order_total * (tip_percentage/Decimal('100'))
+    tip_amount = tip_amount.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    return tip_amount
