8191c14928b29dde6bee37d81c1bc83a30411a0f
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-28 22:35:18 +0530

    68355

--- home/models.py
+++ home/models.py
@@ -36,3 +36,12 @@ class DailySpecial(models.Model):
         if specials.exists():
             return specials.order_by('?').first()
         return None
+
+class NutritionalInformation(models.Model):
+    menu_item = models.ForeignKey('MenuItem', on_delete=models.CASCADE, related_name='nutrition')
+    calories = models.IntegerField()
+    protein_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    def __str__(self):
+        return f"{self.menu_item.name} - {self.calories} kcal" 
