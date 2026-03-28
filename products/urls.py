d330afe22d523f1d42c57490a56509524eb4b0e5
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-28 21:57:48 +0530

    68354

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+
+urlpatterns = router.urls
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
