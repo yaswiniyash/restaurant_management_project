87da4412ba0e6abe084b25b45365a224a53ed0ad
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-28 21:20:12 +0530

    68351

--- orders/utils.py
+++ orders/utils.py
@@ -1,6 +1,8 @@
 import string
 import secrets
 from .models import Coupon
+from django.db.models import Sum
+from .models import Order
 
 def generate_coupon_code(length=10):
     characters = string.ascii_uppercase + string.digits
@@ -9,3 +11,9 @@ def generate_coupon_code(length=10):
 
         if not Coupon.objects.filter(code=code).exists:
             return code
+
+
+def get_daily_sales_total(date):
+    orders = Order.objects.filter(Created_at__date=date)
+    total = orders.aggregate(total_sum=Sum('total_price'))['total_sum']
+    return total if total is not None else 0
