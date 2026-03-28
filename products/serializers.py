2c32adf63a98243131216fbe61158011b7fbea1f
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-28 22:06:43 +0530

    68354

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Product
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
         fields = '__all__'
