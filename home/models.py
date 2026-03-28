4de39d8d3f137425d0e345b82176c4a6fdf9b29d
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-28 21:35:14 +0530

    68352

--- home/models.py
+++ home/models.py
@@ -27,7 +27,7 @@ class MenuItem(models.Model):
 class DailySpecial(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_didits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     @staticmethod
