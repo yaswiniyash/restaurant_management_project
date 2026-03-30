3bbc980a3287af4624e10648e8dc60875a7b5b87
Raya Yaswini <yaswiniyash49@gmail.com>
2026-03-30 23:22:54 +0530

    68655

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("categories/",MenuCategoryListView.as_view(),name="menu-categories"),
     path("today-hours/",show_today_hours),
     path('featured-menu/', FeaturedMenuItemListView.as_view(), name='featured-menu')
+    path('api/menu-items/<int:pk>/ingredients/', MenuIngredientsView.as_view(), name='menu-item-ingredients'),
 ]
