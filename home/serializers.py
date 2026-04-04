c449c53a56511acc58a00c44050ba1c4f13d6b68
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-04 23:03:50 +0530

    69124

--- home/serializers.py
+++ home/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from .models import MenuCategory, MenuItem, Ingredient
+from .models import MenuCategory, MenuItem, Ingredient, Table
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = "__all__" 
+
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +26,4 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ['id', 'name']
+
