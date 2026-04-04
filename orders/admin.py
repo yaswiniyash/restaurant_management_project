19ce5c0691d393b8296fe0e3ad6a1f68e6087628
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-04 23:42:06 +0530

    69126

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+def mark_orders_processed(modeladmin, request, queryset):
+    queryset.update(status='Processed')
+
+class OrderAdmin(admin.Modeladmin):
+    list_display = ['id', 'status']
+    actions = [mark_orders_processed]
+
+admin.site.Register(Order, OrderAdmin)
+
