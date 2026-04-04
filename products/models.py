b0b5fe3cdc64184a3e2cc6e9df1f6765c9c355ba
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-04 22:40:06 +0530

    69123

--- products/models.py
+++ products/models.py
@@ -9,8 +9,10 @@ class Item(models.Model):
     def __str__(self):
         return str(self.item_name)
 
+class category(models.Model):
+    category_name = models.CharField(max_length=100)
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-
-class 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True,blank=True)
