203ab3a28897de50a0b7ca7e52b11833b122e67f
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-12 00:38:58 +0530

    69517

--- orders/models.py
+++ orders/models.py
@@ -14,11 +14,21 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True,null=True,blank=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def get_unique_item_names(self):
+        unique_names = set()
+
+        for item in self.items.all():
+            unique_names.add(item.menu_item.name)
+
+        return list(unique_names)
+
     def __str__(self):
         return f"Order #{self.id} - Status{self.status}"
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    menu_item = models.ForeignKey('product.MenuItem', on_delete=models.CASCADE)
     product_name = models.CharField(max_length=255)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
