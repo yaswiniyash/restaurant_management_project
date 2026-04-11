4ff755cded03c5e20371df168a213eb437dff2e2
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-12 01:54:58 +0530

    69878

--- account/models.py
+++ account/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class LoyaltyProgram(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    points_required = models.IntegerField(
+        unique=True
+    )
+
+    discount_percentage = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
