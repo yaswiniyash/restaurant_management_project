b5619055ca05430fad560e0da9c0ddc9b9b8c867
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-12 00:07:35 +0530

    69514

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class OrderStatus(models.Model):
@@ -8,6 +9,7 @@ class OrderStatus(models.Model):
 
 
 class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete-models.CASCADE,null=True, blank=True)
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True,blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True,null=True,blank=True)
