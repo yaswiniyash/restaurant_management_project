77716f9297cc0c1f438523ea81e358643f65bc12
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-11 23:45:33 +0530

    69513

--- orders/utils.py
+++ orders/utils.py
@@ -4,6 +4,47 @@ from .models import Coupon
 from django.db.models import Sum
 from .models import Order
 from decimal import Decimal, ROUND_HALF_UP
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+logger = logging.getLogger(__name__)
+
+def send_order_confirmation_email(order_id, customer_email, customer_name=None, items=None, total_amount=None):
+    try:
+        try:
+            validate_email(customer_email)
+        except ValidationError:
+            return {"status": "error", "message": "Invalid email"}
+        subject = f"Order Confirmation - #{order_id}"
+        items_text = "\n".join([f"-{item}" for item in items]) if items else "N/A"
+        message = f""" Hi {customer_name or "Customer"},
+
+            Your order has bben placed successfully!
+
+            Order ID:{order_id}
+
+            Items:
+            {items_text}
+
+            Total Amount: {total_amount if total_amount else 'N/A'}
+
+            We'll nofity you once your order is ready.
+
+            Thank you for ordering with us!
+
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email],
+            fail_silently=False,
+        )
+        return {"status": "success", "message": "Email sent"}
+    except Exception as e:
+        logger.error(f"Order Email Error (Order {order_id}): {str(e)}")
+        return {"status": "error", "message": "Email failed"}
 
 def generate_coupon_code(length=10):
     characters = string.ascii_uppercase + string.digits
@@ -25,3 +66,5 @@ def calculate_tip_amount(order_total, tip_percentage):
     tip_amount = order_total * (tip_percentage/Decimal('100'))
     tip_amount = tip_amount.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
     return tip_amount
+
+
