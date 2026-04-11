2222c4ec7e9916cc5f943d49ac4ba3a182805ad8
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-12 00:12:20 +0530

    69514

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("coupons.validate/", CouponValidationView.as_view(),name="validate-coupon"),
     path("history/", OrderHistoryView.as_view, name='order-history'),
     path('coupons/', CouponListView.as_view(), name='coupons'),
+    path('orders/<int:id>/', OrderDetailAPIView.as_view(), name='order_detail'),
 ]
