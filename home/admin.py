89d87dae4d09d842044d153d166ac14f29e6519b
Raya Yaswini <yaswiniyash49@gmail.com>
2026-04-12 00:26:54 +0530

    69516

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from .models import Restaurant
 
 # Register your models here.
-admin.site.register('Restaurant')
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'phone_number', 'email']
+    search_fields = ['name', 'address']
+    list_filter = ['is_active']
+
+    
+admin.site.register(Restaurant, RestaurantAdmin)
+ 
